a04963f44e1a17cd02bda1f9584394aa8c6d535e
olveraas <aolvera@icumed.com>
2020-09-24 18:56:11 -0600

    update

--- lesson one/script/script.js
+++ lesson one/script/script.js
@@ -1,4 +1,1 @@
-var months = ["Janurary", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
-let modified = new Date(document.lastModified);
-let formattedDate = `Last updated: ${months[modified.getMonth()]} ${modified.getDate()}, ${modified.getUTCFullYear()}`;
-document.getElementById("modified").innerHTML = formattedDate;
+document.write("Last Updated: " + document.lastModified +""); 
